6785b9ace72c2492cdeca52fde8b4b8ab2d3f5fc
mahmoud0020 <68845990+mahmoud0020@users.noreply.github.com>
2021-11-08 15:39:01 +0200

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,47 +1,4 @@
-// import {IsValidURL} from './checkURL'
 
-// function handleSubmit(event) {
-//     event.preventDefault()
-
-//     // check what text was put into the form field
-//     let formText = document.getElementById('article-url').value
-
-//     if(Client.IsValidURL(formText)) {
-//     console.log("::: Form Submitted :::")
-
-//     postData('http://localhost:8060/api', {url: formText})
-
-//     .then(function(res) {
-//         document.getElementById('polarity').innerHTML = 'Polarity: '+polarityChecker(res.score_tag);
-//         document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
-//         document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
-//         document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
-//         document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
-//     })
-//     } else {
-//         alert('Seems like an invalid URL, please try with a valid URL.');
-//     }
-// }
-
-// const postData = async (url = "", data = {}) => {
-//     console.log('Analyzing:', data);
-//     const response = await fetch(url, {
-//         method: 'POST',
-//         credentials: 'same-origin',
-//         mode: 'cors',
-//         headers: {
-//         'Content-Type': 'application/4 json',
-//         },
-//         body: JSON.stringify(data)
-//     });
-//     try {
-//         const newData = await response.json();
-//         console.log('Data received:', newData)
-//         return newData;
-//     } catch (error) {
-//         console.log('error', error);
-//     }
-// };
 const handleSubmit =(e)=>{
     e.preventDefault();
     //here i get value of url of input 
@@ -52,9 +9,6 @@ const handleSubmit =(e)=>{
         DataAnalysis({url:TextInput}).then((res)=>{
             updateUI(res);
         })
-        
-        
-       
     }
     else{
         alert("InValid URL please enter correct URL");
